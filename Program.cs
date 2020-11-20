bc4755e06f2898686504beb084c5acee7d82ba10
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-11-20 17:09:01 -0300

    Este commit exibe a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado;
+            
+            Console.Write("Digite o número da tabuada de multiplicação desejada: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for(int x = 1; x <= 10; x++)
+            {
+                resultado = numero * x;
+                //concatenar o número digitado, multiplicador e resultado add sinais "x" E "="
+                Console.WriteLine($"{numero}"+"x"+x+ " = "+ resultado);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Obrigado por utilizar nossa tabuada");
+        }
+    }
+}
